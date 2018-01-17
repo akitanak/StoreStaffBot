2d406bbc7dfb064cb1813ca5d6c4c5a01db5caf9
Akihito <akihito_tanaka@me.com>
2018-01-18 01:38:03 +0900

    Logging is hidden by akka.event.Logging

--- chat-if/src/main/scala/com/github/akitanak/storestaffbot/chatif/util/ActorLogging.scala
+++ chat-if/src/main/scala/com/github/akitanak/storestaffbot/chatif/util/ActorLogging.scala
@@ -1,8 +1,7 @@
 package com.github.akitanak.storestaffbot.chatif.util
 
 import akka.actor.Actor
-import akka.event.Logging
 
 trait ActorLogging extends Actor {
-  val logger = Logging(context.system, this)
+  val logger = akka.event.Logging(context.system, this)
 }
